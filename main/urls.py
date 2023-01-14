251551ec4d9a1041dea64d27176c201e58b94a0a
swayminin <swayminin@yandex.ru>
2023-01-15 01:22:32 +0500

    Новые страницы

--- main/urls.py
+++ main/urls.py
@@ -2,6 +2,9 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index),
-    path('about', views.about)
+    path('', views.index, name='home'),
+    path('demand', views.demand, name='demand'),
+    path('geography', views.geography, name='geography'),
+    path('skills', views.skills, name='skills'),
+    path('latest', views.latest, name='latest'),
 ]
--- main/views.py
+++ main/views.py
@@ -2,8 +2,22 @@ from django.shortcuts import render
 
 
 def index(request):
-    return render(request, 'main/index.html')
+    return render(request, 'main/index.html', {'title': 'C/C++ Разработчик'})
 
 
-def about(request):
-    return render(request, 'main/about.html')
+def demand(request):
+    return render(request, 'main/demand.html', {'title': 'Востребованность'})
+
+
+def geography(request):
+    return render(request, 'main/geography.html', {'title': 'География'})
+
+
+def skills(request):
+    return render(request, 'main/skills.html', {'title': 'Навыки'})
+
+
+def latest(request):
+    return render(request, 'main/latest.html', {'title': 'Последние вакансии'})
+
+
